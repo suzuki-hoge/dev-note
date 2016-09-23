e23d0da200694f7e922d3e20f5457ef6bd5c9faf
suzuki-hoge <user.ryo@gmail.com>
2016-09-23 19:10:52 +0900

    complete completion

---
+++ fab-complete/README.md
@@ -0,0 +1,6 @@
+http://hakobe932.hatenablog.com/entry/2012/02/13/214934
+http://blog.glidenote.com/blog/2012/05/03/zsh-completion/
+http://news.mynavi.jp/column/zsh/018/
+https://askql.wordpress.com/2011/01/11/zsh-writing-own-completion/
+http://wiki.fdiary.net/zsh/?%CA%E4%B4%B0%A5%D5%A5%A1%A5%A4%A5%EB
+https://gist.github.com/mitukiii/4954559
--- fab-complete/def.zsh
+++ fab-complete/def.zsh
@@ -5,15 +5,24 @@ function _fab_tasks {
     IFS_BK=$IFS
     IFS=$'\n'
 
-    fab --list | while read line
+    fabfile=`echo $BUFFER | awk '{for(i=1; i <= NF; i++) if($i == "-f") print $(i + 1) }'`
+
+    if [ -n "$fabfile" ]; then list=`fab -f $fabfile --list`; else list=`fab --list`; fi
+
+    while read line
     do
+        if [[ $line = '' ]] ;then
+            continue
+        fi
         if [[ $in_header -ne 1 ]] ;then
-            tasks+=(`echo $line | awk '{printf("%s", $1); $1=""; printf("[%s]", $0)}' | sed -e 's/\[ /\[/g'`)
+            tasks+=(`echo $line | awk '{printf("%s", $1); $1=""; if ($0 != "") printf("[%s]", $0)}' | sed -e 's/\[ /\[/g'`)
         fi
         if [[ $line =~ 'Available commands:' ]]; then
             in_header=0
         fi
-    done
+    done <<END
+$list
+END
 
     _values 'tasks' $tasks
     IFS=$IFS_BK
